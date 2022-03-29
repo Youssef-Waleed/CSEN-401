794bb0b1430ba2ef0d0c90da6e9ff93bc817c4fa
Greek-Kopis <mahmoudgkasrawi@gmail.com>
2022-03-29 18:14:10 +0200

    lmao

---
+++ src/exceptions/AbilityUseException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class AbilityUseException extends GameActionException {
+	public AbilityUseException() {
+		super();
+	}
+
+	public AbilityUseException(String s) {
+		super(s);
+	}
+}
