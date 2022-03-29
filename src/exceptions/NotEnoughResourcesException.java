a3833d99a980b768725adb978b9eaa5abc6b74d9
Youssef-Waleed <yousefwaleed127@gmail.com>
2022-03-29 18:16:27 +0200

    tests

---
+++ src/exceptions/NotEnoughResourcesException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class NotEnoughResourcesException extends GameActionException {
+	public NotEnoughResourcesException()
+	{
+		super();
+	}
+	public NotEnoughResourcesException(String s)
+	{
+		super(s);
+	}
+}
