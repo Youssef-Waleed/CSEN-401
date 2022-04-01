da994618cd8495fe4862742c0d7e2493c3b7c8df
Youssef-Waleed <yousefwaleed127@gmail.com>
2022-04-01 19:07:36 +0200

    green AND clean

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -97,76 +97,63 @@ public class Game {
 	public static void loadAbilities(String filePath) throws Exception 
 	{
 		BufferedReader br = new BufferedReader(new FileReader(filePath));
+		String[] CSV;
 		String Line=br.readLine();
-		ArrayList<String> ss = new ArrayList<>();
-		while(Line  != null)
+		while(Line!=null)
 		{
-			ss.add(Line);
+			CSV=Line.split(",");
+			if(CSV.length>=8){
+			    Effect effect=null;
+			    if(CSV.length>=9){
+			        switch(CSV[7]){
+			        case"Shield":effect= new Shield(Integer.parseInt(CSV[8]));break; 
+			        case"PowerUp":effect= new PowerUp(Integer.parseInt(CSV[8]));break;
+			        case"Shock":effect= new Shock(Integer.parseInt(CSV[8]));break;
+			        case"Silence":effect= new Silence(Integer.parseInt(CSV[8]));break;
+			        case"SpeedUp":effect= new SpeedUp(Integer.parseInt(CSV[8]));break;
+			        case"Stun":effect= new Stun(Integer.parseInt(CSV[8]));break;
+			        case"Embrace":effect= new Embrace(Integer.parseInt(CSV[8]));break;
+			        case"Root":effect= new Root(Integer.parseInt(CSV[8]));break;
+			        case "Dodge": effect= new Dodge(Integer.parseInt(CSV[8]));break;
+			        case "Disarm": effect= new Disarm(Integer.parseInt(CSV[8]));break;
+			        }
+			    }
+			    switch(CSV[0]){
+			    case "CC":availableAbilities.add(new CrowdControlAbility(CSV[1],Integer.parseInt(CSV[2]),Integer.parseInt(CSV[4]),Integer.parseInt(CSV[3]),AreaOfEffect.valueOf(CSV[5]),Integer.parseInt(CSV[6]),effect));break;
+			    case "HEL":availableAbilities.add(new HealingAbility(CSV[1],Integer.parseInt(CSV[2]),Integer.parseInt(CSV[4]),Integer.parseInt(CSV[3]),AreaOfEffect.valueOf(CSV[5]),Integer.parseInt(CSV[6]),Integer.parseInt(CSV[7])));break;
+			    case "DMG":availableAbilities.add(new DamagingAbility(CSV[1],Integer.parseInt(CSV[2]),Integer.parseInt(CSV[4]),Integer.parseInt(CSV[3]),AreaOfEffect.valueOf(CSV[5]),Integer.parseInt(CSV[6]),Integer.parseInt(CSV[7])));break;
+			    }
+			}
 			Line=br.readLine();
 		}
-		String[][] DataBase=new String[ss.size()][9];
-		for(int i = 0;i<ss.size();i++){
-		     DataBase[i]=ss.get(i).split(",");
-		}
-		for(int i=0;i<DataBase.length;i++)
-		{
-			if(DataBase[i][0]==null||DataBase[i][1]==null||DataBase[i][2]==null||DataBase[i][3]==null||DataBase[i][4]==null||DataBase[i][5]==null||DataBase[i][6]==null||DataBase[i][0]=="CC"&&DataBase[i][7]==null)
-				break;
-			Effect effect=null;
-			switch(DataBase[i][7]){
-			case"Shield":effect= new Shield(Integer.parseInt(DataBase[i][8]));break; 
-			case"PowerUp":effect= new PowerUp(Integer.parseInt(DataBase[i][8]));break;
-			case"Shock":effect= new Shock(Integer.parseInt(DataBase[i][8]));break;
-			case"Silence":effect= new Silence(Integer.parseInt(DataBase[i][8]));break;
-			case"SpeedUp":effect= new SpeedUp(Integer.parseInt(DataBase[i][8]));break;
-			case"Stun":effect= new Stun(Integer.parseInt(DataBase[i][8]));break;
-			case"Embrace":effect= new Embrace(Integer.parseInt(DataBase[i][8]));break;
-			case"Root":effect= new Root(Integer.parseInt(DataBase[i][8]));break;
-			case "Dodge": effect= new Dodge(Integer.parseInt(DataBase[i][8]));break;
-			case "Disarm": effect= new Disarm(Integer.parseInt(DataBase[i][8]));break;
-			}
-			switch(DataBase[i][0]){
-			case "CC":availableAbilities.add(new CrowdControlAbility(DataBase[i][1],Integer.parseInt(DataBase[i][2]),Integer.parseInt(DataBase[i][4]),Integer.parseInt(DataBase[i][3]),AreaOfEffect.valueOf(DataBase[i][5]),Integer.parseInt(DataBase[i][6]),effect));break;
-			case "HEL":availableAbilities.add(new HealingAbility(DataBase[i][1],Integer.parseInt(DataBase[i][2]),Integer.parseInt(DataBase[i][4]),Integer.parseInt(DataBase[i][3]),AreaOfEffect.valueOf(DataBase[i][5]),Integer.parseInt(DataBase[i][6]),Integer.parseInt(DataBase[i][7])));break;
-			case "DMG":availableAbilities.add(new DamagingAbility(DataBase[i][1],Integer.parseInt(DataBase[i][2]),Integer.parseInt(DataBase[i][4]),Integer.parseInt(DataBase[i][3]),AreaOfEffect.valueOf(DataBase[i][5]),Integer.parseInt(DataBase[i][6]),Integer.parseInt(DataBase[i][7])));break;
-			}
-		}
+		br.close();
 	}
 	public static void loadChampions(String filePath) throws Exception
 	{
 		BufferedReader br = new BufferedReader(new FileReader(filePath));
-		
+		String[] CSV;
 		String Line=br.readLine();
-		ArrayList<String> ss = new ArrayList<>();
-		while(Line  != null)
+		for(int i=0;Line!=null;i++)
 		{
-			ss.add(Line);
-			Line=br.readLine();
-		}
-		String[][] DataBase=new String[ss.size()][11];
-		for(int i = 0;i<ss.size();i++){
-		     DataBase[i]=ss.get(i).split(",");
-		}
-		for(int i=0;i<DataBase.length;i++)
-		{
-			if(DataBase[i][0]==null||DataBase[i][1]==null||DataBase[i][2]==null||DataBase[i][3]==null||DataBase[i][4]==null||DataBase[i][5]==null||DataBase[i][6]==null||DataBase[i][7]==null||DataBase[i][8]==null||DataBase[i][9]==null||DataBase[i][10]==null)
+			CSV=Line.split(",");
+			if(CSV.length<11)
 				break;
-			switch(DataBase[i][0])
+			switch(CSV[0])
 			{
-			case "A": availableChampions.add(new AntiHero(DataBase[i][1],Integer.parseInt(DataBase[i][2]),Integer.parseInt(DataBase[i][3]),Integer.parseInt(DataBase[i][4]),Integer.parseInt(DataBase[i][5]),Integer.parseInt(DataBase[i][6]),Integer.parseInt(DataBase[i][7])));break;
-			case "H": availableChampions.add(new Hero(DataBase[i][1],Integer.parseInt(DataBase[i][2]),Integer.parseInt(DataBase[i][3]),Integer.parseInt(DataBase[i][4]),Integer.parseInt(DataBase[i][5]),Integer.parseInt(DataBase[i][6]),Integer.parseInt(DataBase[i][7])));break;
-			case "V": availableChampions.add(new Villain(DataBase[i][1],Integer.parseInt(DataBase[i][2]),Integer.parseInt(DataBase[i][3]),Integer.parseInt(DataBase[i][4]),Integer.parseInt(DataBase[i][5]),Integer.parseInt(DataBase[i][6]),Integer.parseInt(DataBase[i][7])));break;
+			case "A": availableChampions.add(new AntiHero(CSV[1],Integer.parseInt(CSV[2]),Integer.parseInt(CSV[3]),Integer.parseInt(CSV[4]),Integer.parseInt(CSV[5]),Integer.parseInt(CSV[6]),Integer.parseInt(CSV[7])));break;
+			case "H": availableChampions.add(new Hero(CSV[1],Integer.parseInt(CSV[2]),Integer.parseInt(CSV[3]),Integer.parseInt(CSV[4]),Integer.parseInt(CSV[5]),Integer.parseInt(CSV[6]),Integer.parseInt(CSV[7])));break;
+			case "V": availableChampions.add(new Villain(CSV[1],Integer.parseInt(CSV[2]),Integer.parseInt(CSV[3]),Integer.parseInt(CSV[4]),Integer.parseInt(CSV[5]),Integer.parseInt(CSV[6]),Integer.parseInt(CSV[7])));break;
 			}
 			int A1 = -1;
 			int A2 = -1;
 			int A3 = -1;
 			for(int j=0;j<availableAbilities.size();j++)
 			{
-				if(DataBase[i][8].equals(availableAbilities.get(j).getName()))
+				if(CSV[8].equals(availableAbilities.get(j).getName()))
 					A1=j;
-				if(DataBase[i][9].equals(availableAbilities.get(j).getName()))
+				if(CSV[9].equals(availableAbilities.get(j).getName()))
 					A2=j;
-				if(DataBase[i][10].equals(availableAbilities.get(j).getName()))
+				if(CSV[10].equals(availableAbilities.get(j).getName()))
 					A3=j;
 			}
 			if(A1 != -1)
@@ -175,7 +162,9 @@ public class Game {
 				availableChampions.get(i).getAbilities().add(availableAbilities.get(A2));
 			if(A3 != -1)
 				availableChampions.get(i).getAbilities().add(availableAbilities.get(A3));
+            Line=br.readLine();
 		}
+		br.close();
 		
 	}
 	
